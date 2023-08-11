b58ce560112587b06a05c292e83f2ee3cff3112c
Romy <romane.pld@gmail.com>
2023-08-11 18:12:45 +0200

    Header and Footer components + css + responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
+import logo from '../assets/img/logo-white.svg'
+import '../style/footer.css'
+
 export default function Footer () {
     return (
-        <p>FOOTER</p>
+        <footer>
+            <img src={logo} alt='logo'/>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
     )
 }  
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import logo from '../assets/img/logo-pink.svg'
+import '../style/header.css'
+
 export default function Header () {
     return (
         <header>
+            <img src={logo} alt='logo'/>
             <nav>
                 <ul>
                     <li>
