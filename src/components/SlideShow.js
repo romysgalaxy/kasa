0ffa87348a8c04eaec84b6847d38a63e55cc2fca
Romy <romane.pld@gmail.com>
2023-08-13 12:11:29 +0200

    SlideShow component + css + responsive

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,7 +1,37 @@
+import chevronLeft from '../assets/img/chevron_left.svg'
+import chevronRight from '../assets/img/chevron_right.svg'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import data from "../data/data.json";
+import '../style/slideShow.css'
+
 export default function SlideShow() {
+
+  const { itemId } = useParams();
+  const item = data.find((item) => item.id === itemId);
+  const pictures = item.pictures
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? pictures.length - 1 : prevIndex - 1));
+  };
+
+  const goToNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === pictures.length - 1 ? 0 : prevIndex + 1));
+  };
+
     return (
-      <div>
-        SlideShow
+      <div className="slideShow"> 
+        <img src={pictures[currentIndex]} alt={`ShowSlide n°${currentIndex + 1}`} />
+        <div className="slideShow_btn">
+          <button onClick={goToPrevious}>
+            <img src={chevronLeft} alt="Previous"/>
+          </button>
+          <button onClick={goToNext}>
+            <img src={chevronRight} alt="Next"/>
+          </button>
+        </div>
       </div>
     );
   }
